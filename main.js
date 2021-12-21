f139de6637122d45677687ba02c13a7bd73728ec
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2021-12-21 16:33:19 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 video = "";
 status = "";
+objects = [];
 
 
 function preload() {
@@ -19,6 +20,26 @@ function setup() {
 
 function draw() {
    image( video, 0, 0, 480, 380);
+
+   if(status != "") {
+     objectDetector.detect(video, gotResult);
+
+     for(i = 0; i <objects.length; i++) {
+         document.getElementById("status").innerHTML = "Status: Objects Detected";
+         document.getElementById("number_of_objects").innerHTML = "Number Of Objects Are: " + objects.length;
+ 
+         fill("#1dc4b9");
+         percent = floor(objects[i].confidence * 100);
+         text(objects[i].label + "" + percent + "%" , objects[i].x + 15, objects[i].y + 15);
+         noFill();
+         stroke("#1dc4b9");
+         rect( objects[i].x, objects[i].y, objects [i].width, objects[i].height);
+         
+
+     }
+   }
+
+  
 }
 
 
@@ -37,3 +58,16 @@ console.log("Model Loaded");
  video.speed(1);
  video.volume(0);
 }
+
+
+
+function gotResults( error, results) {
+     
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+
